e7ca52e9f59675b54d705ecba8642b0a215088b5
Jordan Hansen <tjhansel@gmail.com>
2023-01-03 18:38:33 -0700

    Updated axios points and added AddToLibrary function

--- server/server.js
+++ server/server.js
@@ -1,62 +1,54 @@
-const express = require('express')
-const cors= require('cors')
-const path = require('path')
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 // app.use(express.static(`/public`))
-app.use(express.static(`./public`))
+app.use(express.static(`./public`));
 
-const gameDatabase = [
-    {
-        title:"Game1",
-        imgURL:"https://cf.geekdo-images.com/fjE7V5LNq31yVEW_yuqI-Q__imagepage/img/ijYTk6KGtxLRdIvLsGar13ZHs4c=/fit-in/900x600/filters:no_upscale():strip_icc()/pic3918905.png"
-    
-    },
-    {
-        title:"Game2",
-        imgURL:"https://cf.geekdo-images.com/yLZJCVLlIx4c7eJEWUNJ7w__imagepagezoom/img/yS4vL6iTCvHSvGySxyOjV_-R3dI=/fit-in/1200x900/filters:no_upscale():strip_icc()/pic4458123.jpg"
-    }
-]
-const myLibrary =[{
-    title:"Wingspan",
-    imgURL:"https://cf.geekdo-images.com/yLZJCVLlIx4c7eJEWUNJ7w__imagepagezoom/img/yS4vL6iTCvHSvGySxyOjV_-R3dI=/fit-in/1200x900/filters:no_upscale():strip_icc()/pic4458123.jpg"
-}]
+const gameDatabase = require("./db.json");
+const myLibrary = [
+  {
+    name: "Wingspan",
+    image:
+      "https://cf.geekdo-images.com/yLZJCVLlIx4c7eJEWUNJ7w__imagepagezoom/img/yS4vL6iTCvHSvGySxyOjV_-R3dI=/fit-in/1200x900/filters:no_upscale():strip_icc()/pic4458123.jpg",
+    thumbnail:
+      "https://cf.geekdo-images.com/yLZJCVLlIx4c7eJEWUNJ7w__thumb/img/VNToqgS2-pOGU6MuvIkMPKn_y-s=/fit-in/200x150/filters:strip_icc()/pic4458123.jpg",
+    minPlayers: 1,
+    maxPlayers: 5,
+    playingTime: 70,
+  },
+];
 
-app.get('/', (req,res) => {
-    res.status(200).sendFile(path.join(__dirname, '../public/index.html'))
-    
-})
-app.get("/api/search", (req,res)=>{
-    const keyWord = req.query.search
-    console.log(keyWord)   
-    
-    const filteredResults = gameDatabase
-    .filter(item => item.title === keyWord)
-    
-    res.json(filteredResults)
-})
-app.post("/api/games", (req,res)=>{
-    const gameToAdd = req.body
-    myLibrary.push(gameToAdd)
-    res.json(myLibrary)
-})
+app.get("/", (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, "../public/index.html"));
+});
+app.get("/api/search", (req, res) => {
+  const keyWord = req.query.search;
+  console.log(keyWord);
 
-app.get("/api/library", (req,res)=>{
-    res.json(myLibrary)
-})
-// const {
-//     getGame,
-//     deleteGame, 
-//     createGame, 
-//     updateGame
-// } = require('./controller')
+  const filteredResults = gameDatabase.filter((item) => item.name === keyWord);
+  res.json(filteredResults);
+});
+app.post("/api/games", (req, res) => {
+  const gameToAdd = req.body;
+  myLibrary.push(gameToAdd);
+  res.json(myLibrary);
+});
 
-// app.get(`/server/controller`, getGameBox)
-// app.delete(`/server/controller`, deleteGame)
-// app.post(`/server/controller`, createGame)
-// app.put(`/server/controller`, updateGame)
+app.get("/api/library", (req, res) => {
+  res.json(myLibrary);
+});
 
-app.listen(4000, console.log("Server Running on 4000"))
+// app.delete("/api/library", (req, res) => {
+//   let deleteGame = myLibrary.name((item) => item.name === +req.params.name);
+//   myLibrary.splice(toDelete, 1);
+//   res.json(myLibrary);
+// });
+
+// <button id="delete" onclick="if(confirm('Are you sure?')) deleteGame(${myLibrary.name});"><i class="ri-delete-bin-line"></i></button>
+
+app.listen(4000, console.log("Server Running on 4000"));
